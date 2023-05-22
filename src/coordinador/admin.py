4e531d72fb843c51389493c7f0a896b42b10aee8
Pablo Camacho <pablocamacho92@gmail.com>
2023-05-22 10:25:27 -0300

    Tarjeta SC1S3-20 Registro Modelo coordinador en Admin

--- src/coordinador/admin.py
+++ src/coordinador/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from .models import Coordinador
 
 # Register your models here.
+class Coordinadoradmin(admin.ModelAdmin):
+    list_display=('nombre','apellido')
+    search_fields=('nombre',)
+    list_filter=('activo',)
 
-admin.site.register(Coordinador)
+admin.site.register(Coordinador,Coordinadoradmin)
