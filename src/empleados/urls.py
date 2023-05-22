38df7dff884e4a65cbc3b36229f552c7660e7e8e
Fernando Zelaya <fernandozelaya9@gmail.com>
2023-05-22 16:04:57 -0300

    Merge con rama_dc para comprobar funcionalidad

--- src/empleados/urls.py
+++ src/empleados/urls.py
@@ -7,7 +7,7 @@ app_name = "empleados"
 urlpatterns = [
     path('nuevo/', nuevo_empleado, name='nuevo_empleado'),
     path('modificar/<int:id>', modificar_empleado, name= 'modificar_empleado'),
-    path('activar/<int:id>',activar_empleado, name='activar_empleado')
+    path('activar/<int:id>',activar_empleado, name='activar_empleado'),
     path('desactivar/<int:pk>', desactivar_empleado, name='desactivar_empleado'),
-    path('listado/',listado_empleados,name='listado_empleados'),
+    path('listado/', listado_empleados,name='listado_empleados'),
 ]
