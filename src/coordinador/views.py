c86dd81166efc1390a52d3a58aeb6714a824a8c6
DanielChachagua <danielmchachagua@gmail.com>
2023-05-24 18:28:52 -0300

    ultimo push antes de pasar todo a developer

--- src/coordinador/views.py
+++ src/coordinador/views.py
@@ -4,4 +4,4 @@ from .models import Coordinador
 
 def listado_coodinadores(request):
     coordinadores=Coordinador.objects.all()
-    return render(request,'coodinador/listado_coordinadores.html',{'coodinadores':coordinadores})
+    return render(request,'coordinador/listado_coordinadores.html',{'coodinadores':coordinadores})
