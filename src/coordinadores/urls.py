bebe2c2fe5cdf632fcac7749a2daddc730185b7e
Pablo Camacho <pablocamacho92@gmail.com>
2023-05-25 15:57:29 -0300

    Tarjeta SC1S3-35

--- src/coordinadores/urls.py
+++ src/coordinadores/urls.py
@@ -8,5 +8,6 @@ app_name = "coordinadores"
 urlpatterns = [
     path('coordinadores/activar/<int:id>',activar_coordinador,name='activar_coordinador'),
     path('coordinadores/desactivar/<int:id>',desactivar_coordinador,name='desactivar_coordinador'),
-    path('listado/',listado_coodinadores,name='listado_coodinadores'),
+    path('coordinadores/listado/',listado_coodinadores,name='listado_coodinadores'),
+    path('coordinadores/crear',nuevo_coordinador,name=nuevo_coordinador)
 ]
--- src/coordinadores/views.py
+++ src/coordinadores/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,get_object_or_404,redirect
 from django.urls import reverse
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import Coordinador
-from .forms import EditarFormCoordinador
+from .forms import EditarFormCoordinador, FormCoordinador
 
 # Create your views here.
 
@@ -22,7 +22,35 @@ def desactivar_coordinador(request,id):
 
 def listado_coodinadores(request):
     coordinadores=Coordinador.objects.all()
-    return render(request,'coordinadores/listado_coordinadores.html',{'coodinadores':coordinadores})
+    return render(request,'coordinadores/listado.html',{'coodinadores':coordinadores})
+
+def nuevo_coordinador(request):
+    form=FormCoordinador()
+    mensaje=None
+    coordinador_nuevo=None
+    if request.method=='POST':
+        form=FormCoordinador(request.POST)
+        if form.is_valid():
+            cd=form.cleaned_data
+            coordinador_nuevo=Coordinador.objects.create(nombre=cd['nombre'],
+                                                         apellido=cd['apellido'],
+                                                         numero_documento=cd['numero_documento'],
+                                                         )
+            mensaje="Coordinador Creado con exito"
+        else:
+            form=FormCoordinador()
+        
+        return render(request,'coordinadores/crear.hmtl',{'form':form,
+                                                                      'mensaje':mensaje,
+                                                                      'coordinador_nuevo':coordinador_nuevo,
+                                                                      })
+            
+
+
+
+    
+
+
 
 def modificar_coordinador (request, id):
     coordinador = get_object_or_404(Coordinador, id=id)
@@ -33,4 +61,4 @@ def modificar_coordinador (request, id):
             return HttpResponseRedirect("/coordinador/listado")
     else:
         formulario = EditarFormCoordinador(instance=coordinador)
-    return render(request, 'coodinador/nuevo_coordinador.html', {'form': formulario}) 
+    return render(request, 'coodinador/crear.html', {'form': formulario}) 
