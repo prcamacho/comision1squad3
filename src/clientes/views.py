f42156fb078ec3629919c7a9c06171905c06ad87
Pablo Camacho <pablocamacho92@gmail.com>
2023-05-26 02:23:57 -0300

    Estandarizo un poco el codigo

--- src/clientes/views.py
+++ src/clientes/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404,redirect
 from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from .models import Cliente
 from .forms import FormCliente, EditarFormCliente
 
@@ -8,8 +9,9 @@ def desactivar_cliente(request,id):
     cliente_a_desactivar=get_object_or_404(Cliente,id=id)
     cliente_a_desactivar.activo=False
     cliente_a_desactivar.save()
-    mensaje=f"Cliente {cliente_a_desactivar.nombre} desactivado"
-    return render(request,'clientes/desactivar.html',{'mensaje':mensaje})
+    #mensaje=f"Cliente {cliente_a_desactivar.nombre} desactivado"
+    #return render(request,'clientes/desactivar.html',{'mensaje':mensaje})
+    return redirect(reverse('clintes:listado_clientes'))
 
 def listado_clientes(request):
     clientes=Cliente.objects.all()
