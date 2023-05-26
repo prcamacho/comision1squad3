1c60c8b158e0b2610aa613661bf6f1997360baf3
Pablo Camacho <pablocamacho92@gmail.com>
2023-05-26 15:36:12 -0300

    actualizacion admin de coordinadores

--- src/coordinadores/admin.py
+++ src/coordinadores/admin.py
@@ -3,8 +3,8 @@ from .models import Coordinador
 
 # Register your models here.
 class Coordinadoradmin(admin.ModelAdmin):
-    list_display=('nombre','apellido','activo')
-    search_fields=('nombre',)
-    list_filter=('activo',)
+    list_display=['nombre','apellido','activo']
+    search_fields=['nombre',]
+    list_filter=['activo',]
 
 admin.site.register(Coordinador,Coordinadoradmin)
