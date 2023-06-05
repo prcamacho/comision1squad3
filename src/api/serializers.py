16638c03e10ba3af5dfbc952ed455d90d30642eb
DanielChachagua <danielmchachagua@gmail.com>
2023-06-05 03:29:00 -0300

    agregando endpoint de los extras

--- src/api/serializers.py
+++ src/api/serializers.py
@@ -1,7 +1,25 @@
 from rest_framework import serializers
-from servicios.models import * 
+from servicios.models import *
+from clientes.models import *
+from empleados.models import *
+from coordinadores.models import * 
 
 class ServicioSerializer(serializers.ModelSerializer):
     class Meta:
         model=Servicio
         fields=['id','nombre','precio']
+        
+class ClienteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Cliente
+        fields=['id','nombre','apellido']
+        
+class EmpleadoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Empleado
+        fields=['id','nombre','apellido','numero_legajo']
+        
+class CoordinadorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Coordinador
+        fields=['id','nombre','apellido','numero_documento','fecha_alta']                        
--- src/api/urls.py
+++ src/api/urls.py
@@ -7,4 +7,10 @@ app_name = "api"
 urlpatterns = [
     path('servicios/',lista_servicios,name='lista_servicios'),
     path('servicios/<int:id>',datos_servicio,name='datos_servicio'),
+    path('clientes/',lista_clientes,name='lista_clientes'),
+    path('clientes/<int:id>',datos_cliente,name='datos_cliente'),
+    path('empleados/',lista_empleados,name='lista_empleados'),
+    path('empleados/<int:id>',datos_empleado,name='datos_empleado'),
+    path('coordinadores/',lista_coordinadores,name='lista_coordinadores'),
+    path('coordinadores/<int:id>',datos_coordinador,name='datos_coordinador'),
 ]
