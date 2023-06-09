31f4991c2a56c08198ecf3f2d652e309fe90e9bb
DanielChachagua <danielmchachagua@gmail.com>
2023-06-09 15:04:44 -0300

    agregado links github y linkedin

--- src/coordinadores/models.py
+++ src/coordinadores/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 from datetime import datetime
-
+from django.utils import timezone
 # Create your models here.
 
 class Coordinador(models.Model):
     nombre=models.CharField(max_length=50)
     apellido=models.CharField(max_length=50)
     numero_documento=models.PositiveBigIntegerField()
-    fecha_alta=models.DateTimeField(default=datetime.now())
+    fecha_alta=models.DateTimeField(default=timezone.now())
     activo=models.BooleanField(default=True)
 
     def __str__(self):
